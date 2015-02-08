03ab6ccc8c20501c8d4335d084abe63014937d3f
Pika3323 <derekellis170@yahoo.ca>
2015-02-07 22:15:12 -0500

    Navigation Drawer
    Added a navigation drawer

--- scripts/index.js
+++ scripts/index.js
@@ -22,4 +22,46 @@
     function onResume() {
         // TODO: This application has been reactivated. Restore application state here.
     };
-} )();
+})();
+$('#menu').click(function () {
+    $('menu, scrim').addClass('open')
+})
+$('scrim').click(function () {
+    $('menu, scrim').removeClass('open');
+});
+$('#search').click(function () {
+    $('#first').toggleClass('search');
+    $('#second').removeClass('none');
+    $('#second, #primary').toggleClass('search');
+    $('#search-input').focus();
+    $('#main, #results').toggleClass('search');
+});
+$('#back').click(function () {
+    $('#first').toggleClass('search');
+    $('#second, #primary').toggleClass('search');
+    $('#search-input').val('');
+    setTimeout(function () { $('#second').addClass('none') }, 500);
+    routes = "";
+    stops = "";
+    $('#results ul').delay(500).html('<li class="pre"><a>Search<sub>Enter a route number, or a 4-digit stop number</sub></a></li>')
+    $('#main, #results').toggleClass('search');
+})
+$('#search-clear').click(function () {
+    if ($('#search-input').val() == "") {
+        $('#first').toggleClass('search')
+        $('#second, #primary').toggleClass('search')
+        setTimeout(function () { $('#second').addClass('none') }, 500);
+        routes = ""
+        stops = ""
+        $('#results ul').html('<li class="pre"><a>Search<sub>Enter a route number, or a 4-digit stop number</sub></a></li>')
+        $('#main, #results').toggleClass('search')
+    } else {
+        $('#search-input').val('').focus()
+        $('#results ul').html('<li class="pre"><a>Search<sub>Enter a route number, or a 4-digit stop number</sub></a></li>')
+    }
+})
+function pad(num, size) {
+    var s = num + "";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
